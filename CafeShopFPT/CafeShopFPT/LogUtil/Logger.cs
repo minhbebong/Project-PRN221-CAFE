ca1abdc9a88d715026fc88f60d8740d35f1d48c0
minhndhe151002 <hichic375@gmail.com>
2023-10-10 18:50:19 +0700

    update first

---
+++ CafeShopFPT/CafeShopFPT/LogUtil/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeShopFPT.LogUtil
+{
+    public class Logger
+    {
+        private static volatile Logger instance;
+        private static object syncRoot = new Object();
+
+        public static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Logger));
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Logger();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
